20be9f3d22b12a85a2eb0d916fca54106e04fe5b
Era Shah <erashah001@gmail.com>
2025-06-14 15:52:07 -0400

    Initial Commit

---
+++ Haul/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Haul
+//
+//  Created by Era Shah on 6/14/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Haul/HaulApp.swift
@@ -0,0 +1,17 @@
+//
+//  HaulApp.swift
+//  Haul
+//
+//  Created by Era Shah on 6/14/25.
+//
+
+import SwiftUI
+
+@main
+struct HaulApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
