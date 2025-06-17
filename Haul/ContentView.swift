ee6ba61f853d1ebef65c03183a58054e6eb92602
Era Shah <erashah001@gmail.com>
2025-06-17 15:33:40 -0400

    Log In UI Changes

--- Haul/ContentView.swift
+++ Haul/ContentView.swift
@@ -8,14 +8,52 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var username: String = ""
+    @State private var password: String = ""
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Image("logo")
+                .resizable()
+                .scaledToFit()
+                .frame(width: 100, height: 100)
+                .padding(30)
+            VStack(alignment: .leading, spacing: 5) {
+                Text("USERNAME")
+                    .font(.custom("Poppins-SemiBold", size: 14))
+                TextField("", text: $username)
+                    .padding()
+                    .frame(width: 315, height: 40)
+                    .background(Color.clear)
+                    .overlay(
+                        RoundedRectangle(cornerRadius: 8)
+                                .stroke(Color.black, lineWidth: 1)
+                    )
+            }
+            .padding(.bottom, 16)
+            VStack(alignment: .leading, spacing: 5) {
+                Text("PASSWORD")
+                    .font(.custom("Poppins-SemiBold", size: 14))
+                TextField("", text: $password)
+                    .padding()
+                    .frame(width: 315, height: 40)
+                    .background(Color.clear)
+                    .overlay(
+                        RoundedRectangle(cornerRadius: 8)
+                                .stroke(Color.black, lineWidth: 1)
+                    )
+            }
+            .padding(.bottom, 30)
+            Button(action: {}) {
+                Text("LOG IN ")
+                    .font(.custom("Poppins-SemiBold", size: 20))
+                    .padding()
+                    .frame(width: 315)
+                    .background(Color.black)
+                    .foregroundColor(.white)
+                    .cornerRadius(50)
+            }
         }
-        .padding()
     }
 }
 
